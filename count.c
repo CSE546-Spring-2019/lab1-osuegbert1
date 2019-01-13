163528ba509efb0aa6e0c837cba9672d3f74a742
David Egbert <osuegbert1@gmail.com>
2019-01-12 21:31:14 -0500

    added string matching. need to handle buffer rollover

--- count.c
+++ count.c
@@ -1,35 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main (int argc, char *argv[])
-{
-  int fileSize = 0, occurances = 0;
-  FILE* input;
-  FILE* output;
+#define BUFFER_SIZE 100
 
-  if(argc != 3){
+int main(int argc, char *argv[]) {
+  FILE *input, *output;
+  unsigned char buffer[BUFFER_SIZE];
+  int fileSize = 0, count = 0, itemsRead;
+  int i;
+  char *inputFilename, *searchString, *outputFilename;
+  unsigned char searchChar;
+  int searchStringLength;
+
+  // check input
+  if (argc != 4) {
     printf("Usage: count <input-filename> <search-string> <output-filename>\n");
-    return(-1);
+    return (-1);
   }
-  if((input = fopen(argv[0], "rb")) == NULL){
+  inputFilename = argv[1];
+  searchString = argv[2];
+  outputFilename = argv[3];
+  // open files
+  if ((input = fopen(inputFilename, "rb")) == NULL) {
     perror("Error");
-    printf("Could not open %s\n", argv[0]);
-    return(-1);
+    printf("Could not open %s\n", inputFilename);
+    return (-1);
   }
-  if((output = fopen(argv[2], "wb")) == NULL){
+  if ((output = fopen(outputFilename, "w")) == NULL) {
     perror("Error");
-    printf("Could not open %s\n", argv[2]);
-    return(-1);
+    printf("Could not open %s\n", outputFilename);
+    return (-1);
   }
 
-
-  //close files
-  if((fclose(input)) != 0){
-    perror("Error");
-    printf("Could not close %s\n", argv[0]);
+  searchChar = *searchString & 0xff;
+  searchStringLength = strlen(searchString);
+  while ((itemsRead = fread(buffer, 1, BUFFER_SIZE, input)) > 0) {
+    fileSize += itemsRead;
+    for (i = 0; i < itemsRead; i++) {
+      if (buffer[i] == searchChar &&
+          !memcmp(&buffer[i], searchString, searchStringLength)) {
+        count++;
+      }
+    }
   }
-  if((fclose(output)) != 0){
+
+  printf("Size of file is %i\n", fileSize);
+  fprintf(output, "Size of file is %i\n", fileSize);
+  printf("Number of matches = %i\n", count);
+  fprintf(output, "Number of matches = %i\n", count);
+
+  // close files
+  if ((fclose(input)) != 0) {
     perror("Error");
-    printf("Could not close %s\n", argv[2]);
+    printf("Could not close %s\n", inputFilename);
+  }
+  if ((fclose(output)) != 0) {
+    perror("Error");
+    printf("Could not close %s\n", outputFilename);
   }
 }
